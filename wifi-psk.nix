1de29baf457fa36b0f22d48040b27d403f2241c0
xenia <xenia.agda@gmail.com>
2024-12-21 21:27:13 +0100

    Make the default wifi-psk.nix error to not accidentally kill the wifi connection if it gets restored

--- wifi-psk.nix
+++ wifi-psk.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, modulesPath, ... }:
 {
   # generated via wpa_passphrase "wifi-name"
-  networking.wireless.networks."changeme".pskRaw = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+  networking.wireless.networks."changeme".pskRaw = builtins.abort "set a wifi password";
 }
