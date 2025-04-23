c35cd23e277ce39f4ad9bd3f1b7887cbd87757ad
xenia <xenia.agda@gmail.com>
2025-04-24 00:58:03 +0200

    OOPS. make home-assistant not DISAPPEAR when restarting computer

--- machines/catboy-cafe/machine.nix
+++ machines/catboy-cafe/machine.nix
@@ -28,7 +28,7 @@ in {
   virtualisation.oci-containers = {
     backend = "podman";
     containers.homeassistant = {
-      volumes = [ "home-assistant:/home-assistant" ];
+      volumes = [ "home-assistant:/config" ];
       environment.TZ = "Europe/Berlin";
       image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
       extraOptions = [
